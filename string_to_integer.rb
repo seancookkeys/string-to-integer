3b4a09790692952d91626e7b92a65ecc6ee776ef
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-08-11 23:27:54 +0000

    pushed up what i have

--- string_to_integer.rb
+++ string_to_integer.rb
@@ -1,1 +1,29 @@
-string
+def string_to_integer(input)
+  chars = input.split('')
+  chars.each do |c|
+    puts c
+  end
+end
+
+puts "Enter a string"
+input = gets 
+result = string_to_integer(input)
+puts result
+puts result.instance_of?(Integer)
+
+
+# params 
+
+# we need to take in a string which consistents of integer digits 
+# we are going to hard code in a string 
+# function will return the integer equivalent of the string 
+
+
+
+# takes a string turns it into an integer
+# Given a string representation of a whole number, convert it into an integer without using the to_i method.
+
+# 100 = 10 to the second power
+# 2*10 is basically 10 raised to the 1st power
+
+# integer is 1, 2, 3 = no decimal point 
